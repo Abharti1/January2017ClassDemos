c9c4103f1e3e244374b21ad4ef530061d015fa1e
Abharti1 <abharti1@nait.ca>
2017-01-09 17:29:05 -0700

    New Project

---
+++ ClassDemos/WebSiteJan2017/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSiteJan2017.Startup))]
+namespace WebSiteJan2017
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
